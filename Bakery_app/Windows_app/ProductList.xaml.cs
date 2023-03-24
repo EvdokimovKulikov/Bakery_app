b82d4c95020fa62e17b9e34312d82d7982957209
Имя Фамилия <visualstudio1945@outlook.com>
2023-03-24 10:04:18 +0300

    1

--- Bakery_app/Windows_app/ProductList.xaml.cs
+++ Bakery_app/Windows_app/ProductList.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Bakery_app.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Bakery_app.ClassHelper.EFClass;
 
 namespace Bakery_app.Windows_app
 {
@@ -19,9 +22,39 @@ namespace Bakery_app.Windows_app
     /// </summary>
     public partial class ProductList : Window
     {
+        private void GetListProduct()
+        {
+            List<Product> products = new List<Product>();
+            products = ContextDB.Product.ToList();
+
+
+            products = products.Where(i => i.ProdName.ToLower().Contains(TbSearch.Text.ToLower())).ToList();
+            // поиск, сортировка, фильтрация
+
+            LvProduct.ItemsSource = products;
+        }
         public ProductList()
         {
             InitializeComponent();
+        }
+
+        private void BtnEditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            EditProductList editProductList = new EditProductList();
+            editProductList.Show(); 
+            
+        }
+
+        private void BtnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            EditProductList editProductList = new EditProductList();
+            editProductList.ShowDialog();
+
+        }
+
+        private void TbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            GetListProduct();
         }
     }
 }
